17d37e302aa5fe41d4a440b2abc11882243bb440
Adam Stirtan <adamstirtan@gmail.com>
2024-05-18 22:02:02 -0400

    Local changes

--- NESharp/CPU.cs
+++ NESharp/CPU.cs
@@ -1,6 +1,43 @@
 ﻿namespace NESharp;
+
 public sealed class CPU
 {
+    public byte A { get; set; }
+    public byte X { get; set; }
+    public byte Y { get; set; }
+    public byte _stackPointer { get; set; }
+    public ushort _programCounter { get; set; }
+    public byte _status { get; set; }
+
+    private Memory _memory;
+
+    public CPU(Memory memory)
+    {
+        _memory = memory;
+
+        Reset();
+    }
+
     public void ExecuteNextInstruction()
-    { }
+    {
+        byte opcode = FetchByte();
+    }
+
+    public void Reset()
+    {
+        A = 0;
+        X = 0;
+        Y = 0;
+        _stackPointer = 0xFD;
+        _programCounter = (ushort)(_memory.ReadMemory(0xFFFC) | (_memory.ReadMemory(0xFFFD) << 8));
+    }
+
+    private byte FetchByte()
+    {
+        byte value = ReadMemory(_programCounter);
+
+        _programCounter++;
+
+        return value;
+    }
 }
